1790fb1e1ab27cc4d3514f65b14a81c1c3cab2ab
Michael Lawrence <lawremi@iastate.edu>
2008-10-28 14:12:49 +0000

    Remove largely untested and certainly unused Argo backend. Not worth the maintenance.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/rtracklayer@34944 bc3139a8-67e5-0310-9ffc-ced21a209358

--- load.R
+++ load.R
@@ -4,13 +4,12 @@ library(Biobase)
 library(rJava)
 
 files <- c("web.R", "segment.R", "trackSet.R", "browser.R", "gff.R", "ucsc.R",
-           "bed.R", "wig.R", "io.R", "argo.R")
+           "bed.R", "wig.R", "io.R")
 sapply(files, source)
 
 #track <- import(system.file("inst", "test", "v1.gff", package = "rtracklayer"))
 track <- import("../inst/tests/bed.wig")
 track@genome <- "hg18"
 
-#session <- browseGenome(browser = "argo")
-session <- browserSession("argo")
+session <- browserSession("ucsc")
 layTrack(session, track)
