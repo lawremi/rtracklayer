fec4294cbd3e652e42f3d706c4323154aa939701
Michael Lawrence <lawremi@iastate.edu>
2009-01-17 00:21:35 +0000

    Add microRNA targets dataset and demo (for paper)
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/rtracklayer@36727 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ demo/targets.R
@@ -0,0 +1,40 @@
+###################################################
+### chunk number 1: rtl-init
+###################################################
+library(rtracklayer)
+data(targets)
+
+###################################################
+### chunk number 2: rtl-miRNA-track
+###################################################
+targetTrack <- with(targets, 
+    RangedData(IRanges(start, end), 
+               strand, space = chrom))
+
+
+###################################################
+### chunk number 3: rtl-export eval=FALSE
+###################################################
+## export(targetTrack, "targets.wig")
+
+
+###################################################
+### chunk number 4: rtl-ucsc-start
+###################################################
+session <- browserSession()
+
+
+###################################################
+### chunk number 5: rtl-ucsc-lay
+###################################################
+session$targets <- targetTrack
+
+
+###################################################
+### chunk number 6: rtl-ucsc-view eval=FALSE
+###################################################
+range <- ranges(targetTrack[1,]) * -10
+view <- browserView(session, range,
+                    pack = "targets")
+
+
