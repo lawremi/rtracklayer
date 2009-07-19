9bd1d18ae2ee1a90316ef621084f8cb3343ad824
Michael Lawrence <lawremi@iastate.edu>
2009-07-19 12:27:07 +0000

    Add some testing scripts/files.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/rtracklayer@40707 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ inst/tests/test.R
@@ -0,0 +1,9 @@
+library(rtracklayer)
+files <- dir(system.file("tests", package = "rtracklayer"), pattern = "[^R~]$",
+             full.names=TRUE)
+testExport <- function(file) {
+  track <- import(file)
+  export(track, format = "ucsc")
+}
+options(error=recover)
+sapply(files, testExport)
