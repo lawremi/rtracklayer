8529123c79d0b72fc571f8053871c93c3e5ba653
Michael Lawrence <lawremi@iastate.edu>
2011-10-22 18:39:09 +0000

    Utility tweaks
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/rtracklayer@59545 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/utils.R
+++ R/utils.R
@@ -1,18 +1,9 @@
-normURI <- function(x) {
-  if (!isSingleString(uri))
-    stop("URI must be a single, non-NA string")
-  uri <- parseURI(x)
-  if (!nzchar(uri$scheme))
-    x <- paste("file://", file_path_as_absolute(x), sep = "")
-  x
-}
-
 pasteCollapse <- function(x, collapse = ",", sentinel = "\r") {
   s <- start(PartitioningByWidth(x))
   x_flat <- unlist(x, use.names = FALSE)
   x_flat[s] <- paste(sentinel, x_flat[s], sep = "")
-  big_list <- paste(v, collapse = collapse)
-  gsub_str <- paste(concat_str, sentinel, sep="")
+  big_list <- paste(x_flat, collapse = collapse)
+  gsub_str <- paste(collapse, sentinel, sep="")
   big_list <- gsub(gsub_str, sentinel, big_list, fixed = TRUE)
   big_list <- substring(big_list, nchar(sentinel))
   unlist(strsplit(big_list, sentinel, fixed = TRUE))
