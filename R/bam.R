7d647f3b6e30b2954216380408738be6e74c99c2
Michael Lawrence <lawremi@iastate.edu>
2012-05-23 14:20:54 +0000

    add prototype of bam export
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/rtracklayer@66128 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/bam.R
+++ R/bam.R
@@ -10,3 +10,45 @@ setMethod("import", "BamFile",
               checkArgFormat(con, format)
             readBamGappedAlignments(con, use.names = use.names, param = param)
           })
+
+setMethod("export", c("GappedAlignments", "BamFile"),
+          function(object, con, format) {
+            sam_path <- paste(file_path_sans_ext(path(con)), ".sam", sep = "")
+            sam_con <- file(sam_path, "w")
+            on.exit(close(sam_con))
+            si <- seqinfo(object)
+            has_info <-
+              seqlevels(si)[!is.na(seqlevels(si)) & !is.na(seqlengths(si))]
+            si <- si[has_info]
+            if (length(si)) {
+              header <- paste0("@SQ",
+                               "\tSN:", seqlevels(si),
+                               "\tLN:", seqlengths(si))
+              has_genome <- !is.na(genome(si))
+              header[has_genome] <-  paste0(header[has_genome], "\tAS:",
+                                            genome(si)[has_genome])
+              writeLines(header, sam_con)
+            }
+            emd <- values(object)
+            aln <- paste(if (!is.null(names(object))) names(object) else "*",
+                         ifelse(strand(object) == "-", "16", "0"),
+                         seqnames(object), start(object),
+                         if (!is.null(emd[["mapq"]])) emd[["mapq"]] else "255",
+                         cigar(object),
+                         if (!is.null(emd[["mrnm"]])) emd[["mrnm"]] else "*",
+                         if (!is.null(emd[["mpos"]])) emd[["mpos"]] else "0",
+                         if (!is.null(emd[["isize"]])) emd[["isize"]] else "0",
+                         if (!is.null(emd[["seq"]])) emd[["seq"]] else "*",
+                         if (!is.null(emd[["qual"]])) emd[["qual"]] else "*",
+                         sep = "\t")
+            writeLines(aln, sam_con)
+            close(sam_con)
+            on.exit()
+            asBam(sam_path, file_path_sans_ext(sam_path))
+          })
+
+setMethod("export", c("ANY", "BamFile"),
+          function(object, con, format, ...) {
+            export(as(object, "GappedAlignments"), con, ...)
+          })
+
