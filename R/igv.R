5985f04638de0e6a1b79022fea927542ffaf1755
Michael Lawrence <michafla@gene.com>
2019-06-04 10:49:48 -0700

    add functions for reading an IGV data server registry

---
+++ R/igv.R
@@ -0,0 +1,25 @@
+### =========================================================================
+### IGV Data Server Access
+### -------------------------------------------------------------------------
+
+readIGVRegistry <- function(url) {
+    do.call(rbind, lapply(readLines(url), readIGVDataFile))
+}
+
+handleCategory <- function(x) {
+    paths <- unlist(getNodeSet(x, "Resource/@path"))
+    names(paths) <- unlist(getNodeSet(x, "Resource/@name"))
+    name <- factor(unname(xmlAttrs(x)["name"]))
+    categories <- lapply(getNodeSet(x, "Category"), handleCategory)
+    children <- do.call(rbind, categories)
+    children$parenet <- name
+    resources <- rtracklayer:::RTLFileList(lapply(paths, FileForFormat))
+    df <- DataFrame(category=name, parent=NA, name=names(paths), resources)
+    rbind(df, children)
+}
+
+readIGVDataFile <- function(url) {
+    lines <- readLines(url)
+    doc <- xmlTreeParse(lines, asText=TRUE, useInternalNodes=TRUE)
+    handleCategory(getNodeSet(doc, "/Global")[[1L]])
+}
