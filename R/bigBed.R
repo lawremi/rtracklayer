fbfcc7f47c000e856880edcabdb3ad9fa138b443
Sanchit Saini <sanchitsaini14@gmail.com>
2020-07-30 00:44:23 +0530

    Add BigBedSelection and BBFile class

--- R/bigBed.R
+++ R/bigBed.R
@@ -7,14 +7,32 @@
 ###
 
 setClass("BigBedFile", contains = "RTLFile")
+setClass("BBFile", contains = "BigBedFile")
 
 BigBedFile <- function(path) {
   if (!isSingleString(path))
     stop("'filename' must be a single string, specifying a path")
   new("BigBedFile", resource = path)
 }
+BBFile <- BigBedFile
 
 setMethod("seqinfo", "BigBedFile", function(x) {
   seqlengths <- .Call(BBDFile_seqlengths, expandPath(path(x)))
   Seqinfo(names(seqlengths), seqlengths)
 })
+
+.defaultColNames <- c("name", "score", "thick", "itemRgb", "blocks")
+
+setClass("BigBedSelection", prototype = prototype(colnames = .defaultColNames),
+         contains = "RangedSelection")
+
+BigBedSelection <- function(ranges=IRangesList(), colnames = .defaultColNames) {
+  if (is.character(ranges))
+    new("BigBedSelection", GenomicSelection(ranges, colnames = colnames))
+  else {
+    if (is(ranges, "BigBedFile"))
+      ranges <- seqinfo(ranges)
+    new("BigBedSelection", ranges = as(ranges, "IntegerRangesList"),
+     colnames = colnames)
+  }
+}
