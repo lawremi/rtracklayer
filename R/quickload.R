cfff0243b7005d01ed6fc4343cfa6a29e7030e37
Michael Lawrence <michafla@gene.com>
2021-12-07 13:33:22 -0800

    drop seqinfo,DNAStringSet() since made obsolete by seqinfo,List()

--- R/quickload.R
+++ R/quickload.R
@@ -153,15 +153,6 @@ setMethod("toString", "GenomeDescription", function(x) {
   paste(organism(x), provider(x), providerVersion(x))
 })
 
-## Not sure where this method should land. I'm sure it will be useful
-## for publishing adhoc genomes through Quickload.
-setMethod("seqinfo", "DNAStringSet", function(x) {
-  x_names <- names(x)
-  if (is.null(x_names))
-    x_names <- as.character(seq(length(x)))
-  Seqinfo(x_names, width(x))
-})
-
 QuickloadGenome <- function(quickload, genome, create = FALSE,
                             seqinfo = GenomicRanges::seqinfo(genome),
                             title = toString(genome))
