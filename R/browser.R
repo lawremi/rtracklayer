78a07a5651f728b3a24ea53f73c8ad2dde69f5ed
Sanchit <sanchitsaini14@gmail.com>
2024-05-02 11:49:17 +0530

    Remove login(), saveView() and restoreView()

--- R/browser.R
+++ R/browser.R
@@ -187,12 +187,3 @@ setMethod("browserSession", "missing",
 
 # get one from a view
 setMethod("browserSession", "BrowserView", function(object) object@session)
-
-setGeneric("login",
-           function(x, username, password, ...) standardGeneric("login"),
-           signature = "x")
-
-setGeneric("saveView", function(x, ...) standardGeneric("saveView"))
-
-setGeneric("restoreView",
-           function(x, ...) standardGeneric("restoreView"))
